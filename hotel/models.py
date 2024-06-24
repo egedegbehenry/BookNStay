fc445411de3965c2f6ef91dc3f36285a40a9f375
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-25 02:23:32 +0300

    add templates and update models.py

--- hotel/models.py
+++ hotel/models.py
@@ -17,7 +17,7 @@ class Room(models.Model):
     beds =models.IntegerField()
     capacity = models.IntegerField()
 
-    def __str__(self) :
+    def __str__(self):
         return f'{self.number}. {self.category} with {self.beds}beds for {self.capacity} people'
    
 class Booking(models.Model):
@@ -26,6 +26,6 @@ class Booking(models.Model):
     check_in = models.DateTimeField()
     check_out = models.DateTimeField()
 
-    def __str__(self) :
-        return f'{self.user} haas booked {self.room} from {self.check_in} to {self.check_out}'
+    def __str__(self):
+        return f'{self.user} has booked {self.room} from {self.check_in} to {self.check_out}'
 
