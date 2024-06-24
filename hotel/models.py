547efdf713e337f851a0413cbbced1187990c145
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-24 13:52:42 +0300

    add room and booking in models.py

--- hotel/models.py
+++ hotel/models.py
@@ -19,4 +19,12 @@ class Room(models.Model):
 
     def __str__(self) :
         return f'{self.number}. {self.category} with {self.beds}beds for {self.capacity} people'
-    class Booking
+    class Booking(models.Model):
+        user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE)
+        room = models.ForeignKey(Room, on_delete=models.CASCADE)
+        check_in = models.DateTimeField()
+        check_out = models.DateTimeField()
+
+        def __str__(self) :
+            return f'{self.user} haas booked {self.room} from {self.check_in} to {self.check_out}'
