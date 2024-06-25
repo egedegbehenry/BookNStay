427e4f76245bce1c91bceb57ca24bd85712f267b
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-25 05:33:45 +0300

    add availability, check_in, and checkout details to the form

--- hotel/views.py
+++ hotel/views.py
@@ -26,3 +26,16 @@ class BookingView(FormView):
         for room in room_list:
             if check_availability(room, data['check_in'], data['check_out']):
                 available_rooms.append(room)
+
+        if len(available_rooms)>0:
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user=request.user,
+                room=room,
+                check_in=data['check_in'],
+                check_out=data['check_out']
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse('All of this category of rooms are booked!! Book another one')
