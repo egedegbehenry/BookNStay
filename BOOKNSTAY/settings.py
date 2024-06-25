eabebcc847459b884b9ff4108fcbd9d8227654cd
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-25 03:20:18 +0300

    add booking_functions app

--- BOOKNSTAY/settings.py
+++ BOOKNSTAY/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps start here
     'hotel',
+    'hotel.booking_functions',
+
 ]
 
 MIDDLEWARE = [
