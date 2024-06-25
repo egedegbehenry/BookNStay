bd2c4ddad65993d30e41da207b198d945a29dbe2
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-25 03:42:32 +0300

    add availability options

--- hotel/booking_functions/availabity.py
+++ hotel/booking_functions/availabity.py
@@ -2,4 +2,11 @@ import datetime
 from hotel.models import Room, Booking
 
 
-def check_availability(room,check_in, check_out):
+def check_availability(room, check_in, check_out):
+    avail_list=[]
+    bookings_list = Booking.objects.filter(room=room)
+    for booking in bookings_list:
+        if booking.check_in > check_out or booking.check_out < check_in:
+            avail_list.append(True)
+        else:
+            avail_list.append(False)
