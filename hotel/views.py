d666466afaf65c12efc89bcbd2f05898a78ba895
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-25 05:25:19 +0300

    add availability, check_in, and checkout details to the form

--- hotel/views.py
+++ hotel/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
 from .models import Room, Booking
+from .forms import AvailabilityForm
+from booking_functions.availability import check_availability
 
 # Create your views here.
+
 
 class RoomList(ListView):
     model=Room
 
+
     class BookingList(ListView):
         model=Booking
+
+
+class BookingView(FormView):
+    form_class = AvailabilityForm
+    template_name = 'availability_form.html'
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        room_list =Room.objects.filter(category=data[ 'room_category'])
+        available_rooms=[]
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
