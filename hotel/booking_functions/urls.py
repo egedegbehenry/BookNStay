6f02015ae4312975946015901d25c36e00019c81
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-27 02:35:21 +0300

    add booking list, room list etc to template

--- hotel/booking_functions/urls.py
+++ hotel/booking_functions/urls.py
@@ -5,8 +5,8 @@ app_name = 'hotel'
 
 urlpatterns = [
     path('room_list/', RoomList.as_view(), name='RoomList'),
-    path('booking_list/', RoomList.as_view(), name='BookingList'),
-    path('book/', BookingView.as_view(), name='booking_view'),
+    path('booking_list/', BookList.as_view(), name='BookingList'),
+    path('book/', BookingView.as_view(), name='bookingView'),
     path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
 
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -16,8 +16,34 @@ class RoomListView(ListView):
 
 class RoomView(View):
     def get (self, request, *args, **kwargs):
+        room_category = self.kwargs.get('category',None)
+        room_list = Room.objects.filter(category=category)
+        room = room_category[0]
+        return render()
+       
+
     
     def post(self, request, *args, **kwargs):
+         room_list = Room.objects.filter(category=category)
+
+        available_rooms=[]
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
+
+        if len(available_rooms)>0:
+            room = available_rooms[0]
+            booking = Booking.objects.create(
+                user=self.request.user,
+                room=room,
+                check_in=data['check_in'],
+                check_out=data['check_out']
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse('All of this category of rooms are booked!! Book another one')
+
 
 class BookingDetailView(FormView):
     form_class = AvailabilityForm
