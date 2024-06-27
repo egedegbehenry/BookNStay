8a37b260ac9c2b19c3c4052da2b93d0be08ef3b3
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-27 03:39:59 +0300

    add room description, avaialbilty and other details  and update

--- hotel/booking_functions/urls.py
+++ hotel/booking_functions/urls.py
@@ -4,7 +4,7 @@ from .views import RoomListView, BookingList, BookingView, RoomDetailView
 app_name = 'hotel'
 
 urlpatterns = [
-    path('room_list/', RoomList.as_view(), name='RoomList'),
+    path('room_list/', RoomListView.as_view(), name='RoomList'),
     path('booking_list/', BookList.as_view(), name='BookingList'),
     path('book/', BookingView.as_view(), name='bookingView'),
     path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
--- hotel/views.py
+++ hotel/views.py
@@ -16,19 +16,25 @@ class RoomListView(ListView):
 
 class RoomView(View):
     def get (self, request, *args, **kwargs):
-        room_category = self.kwargs.get('category',None)
-        #room_list = Room.objects.filter(category=category)
+        category = self.kwargs.get('category',None)
+        form = AvailabilityForm()
+        room_list = Room.objects.filter(category=category)
         
-
-        context={
-            'room_category' room_category
-        }
-        return render( request, 'room_detail_view.html', context)
-       
+        if len (room_list) > 0:
+            room = room_list[0]
+            room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
+        
+            context ={
+                'room_category': room_category,
+                'form': form,
+            }
+            return render(request, 'room_detail_view.html', context)
+        else:
+            return HttpResponse('Category is not avaialable')
 
     
     def post(self, request, *args, **kwargs):
-         room_list = Room.objects.filter(category=category)
+        room_list = Room.objects.filter(category=category)
 
         available_rooms=[]
         for room in room_list:
