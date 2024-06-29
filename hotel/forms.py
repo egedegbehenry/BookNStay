9dbdd5e5feca50ae8e4d7f5b4dce027e10a764fc
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-29 03:54:05 +0300

    room availability added to form

--- hotel/forms.py
+++ hotel/forms.py
@@ -4,3 +4,13 @@ from django.core.exceptions import ValidationError
 from .models import RoomCategory, Person
 
 #  input_formats=["%Y-%m-%dT%H:%M", ],
+
+class AvailabilityForm(forms.Form):
+
+    check_in = forms.DateTimeField(
+        required=True, input_formats=["%Y-%m-%dT%H:%M", "%Y-%m-%dT%H:%M%Z"], widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+    check_out = forms.DateTimeField(
+        required=True, input_formats=["%Y-%m-%dT%H:%M", "%Y-%m-%dT%H:%M%Z"], widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
+    room_category = forms.ModelChoiceField(
+        queryset=RoomCategory.objects.all())
+    #  widget=forms.Select(attrs={"class": "mdb-select md-form"})
