d7fadb797735ed18e8c1f9c4f1bc88f6cd9a8845
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-29 03:18:27 +0300

    add app name to hotel urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,25 +1,13 @@
-"""
-URL configuration for BOOKNSTAY project.
+from django.urls import path
+from .views import RoomLisView. BookingList, BookingView, RoomDetaillView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+app_name = 'hotel'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hotel.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('room_list/', RoomLisView.as_view, name='RoomList'),
+    path('booking_list/', BookingList.as_view(), name='BookingList'),
+    path('book/', BookingView.as_view(), name='BookingView'),
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailsView')
+
+
+]
