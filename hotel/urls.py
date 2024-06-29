6ac63e43f13edcd715a0bf8811e21246e028a534
Henry Egedegbe <egedegbehenry@gmail.com>
2024-06-29 03:41:50 +0300

    add necessary path to the urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import RoomLisView. BookingList, BookingView, RoomDetaillView
+from .views import BookingListView, RoomDetailView, CancelBookingView, CheckoutView, success_view, cancel_view, BookingFormView, contact_us
 
 app_name = 'hotel'
 
 urlpatterns = [
-    path('room_list/', RoomLisView.as_view, name='RoomList'),
+    path('room_list/', RoomListView, name='RoomList'),
     path('booking_list/', BookingList.as_view(), name='BookingList'),
     path('book/', BookingView.as_view(), name='BookingView'),
-    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailsView')
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailsView'),
 
 
 ]
