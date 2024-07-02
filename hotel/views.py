24f8fa36b8117bb43682e3cb87154937b9191c7d
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-03 02:54:27 +0300

    update models.py

--- hotel/views.py
+++ hotel/views.py
@@ -31,6 +31,9 @@ def RoomListView(request):
 
 class BookingList(ListView):
     model=Booking
+    template_name="booking_list_view.html"
+
+
     def get_queryset(self, *args, **kwargs):
         if self.request.user.is_staff:
             booking_list = Booking.objects.all()
@@ -88,23 +91,23 @@ class BookingDetailView(FormView):
     form_class = AvailabilityForm
     template_name = 'availability_form.html'
 
-    def form_valid(self, form):
-        data = form.cleaned_data
-        room_list =Room.objects.filter(category=data[ 'room_category'])
-        available_rooms=[]
-        for room in room_list:
-            if check_availability(room, data['check_in'], data['check_out']):
-                available_rooms.append(room)
+    #def form_valid(self, form):
+        #data = form.cleaned_data
+        #room_list =Room.objects.filter(category=data[ 'room_category'])
+        #available_rooms=[]
+        #for room in room_list:
+            #if check_availability(room, data['check_in'], data['check_out']):
+                #available_rooms.append(room)
 
-        if len(available_rooms)>0:
-            room = available_rooms[0]
-            booking = Booking.objects.create(
-                user=self.request.user,
-                room=room,
-                check_in=data['check_in'],
-                check_out=data['check_out']
-            )
-            booking.save()
-            return HttpResponse(booking)
-        else:
-            return HttpResponse('All of this category of rooms are booked!! Book another one')
+        #if len(available_rooms)>0:
+           # room = available_rooms[0]
+            #booking = Booking.objects.create(
+                #user=self.request.user,
+                #room=room,
+                #check_in=data['check_in'],
+                #check_out=data['check_out']
+            #)
+            #booking.save()
+            #return HttpResponse(booking)
+        #else:
+           # return HttpResponse('All of this category of rooms are booked!! Book another one')
