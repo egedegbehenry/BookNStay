7adf5aed8e08219a222f32536a4f666e6642d1c2
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-03 04:53:32 +0300

    create get room cat url list and populate it

--- hotel/booking_functions/get_room_cat_url_list.py
+++ hotel/booking_functions/get_room_cat_url_list.py
@@ -1,11 +1,12 @@
 from hotel.models import Room
 from django.urls import reverse
 
-#function that returns Room List
+#function that returns Room Category and Category URL List
 
 
 def get_room_cat_url_list():
     rooom = Room.objects.all()[0]     #Getting a Random "Room" object
+    
     #Making a dictionary from  "ROOM CATEGORIES" Tuple on the "Room"   
     room_categories = dict(room.ROOM_CATEGORIES)                         
 
@@ -14,7 +15,7 @@ def get_room_cat_url_list():
     for category in room_categories:                                      #for loop for populating the room_cat_url_list  (cat = category)
         room_category = room_categories.get(category)
         room_url = reverse('hotel:RoomDetailView', Kwargs={
-                            'category': room_category})
+                            'category': category})
        
         room_cat_url_list.append((room_category, room_url))
 
