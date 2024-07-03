4d79a1897eba376b1163be6d2e8867ba4f828a92
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-03 03:44:55 +0300

    update booking cancellation option

--- hotel/views.py
+++ hotel/views.py
@@ -2,8 +2,8 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, FormView, View
-from django.urls import reverse
+from django.views.generic import ListView, FormView, View, DeleteView
+from django.urls import reverse, reverse_lazy
 from .models import Room, Booking
 from .forms import AvailabilityForm
 from hotel.booking_functions.availability import check_availability
@@ -29,7 +29,7 @@ def RoomListView(request):
    
     return render(request, 'room_list_view.html', context)
 
-class BookingList(ListView):
+class BookingListView(ListView):
     model=Booking
     template_name="booking_list_view.html"
 
@@ -87,27 +87,9 @@ class RoomView(View):
             return HttpResponse(booking)
         else:
             return HttpResponse('All of this category of rooms are booked!! Book another one')
-class BookingDetailView(FormView):
-    form_class = AvailabilityForm
-    template_name = 'availability_form.html'
-
-    #def form_valid(self, form):
-        #data = form.cleaned_data
-        #room_list =Room.objects.filter(category=data[ 'room_category'])
-        #available_rooms=[]
-        #for room in room_list:
-            #if check_availability(room, data['check_in'], data['check_out']):
-                #available_rooms.append(room)
-
-        #if len(available_rooms)>0:
-           # room = available_rooms[0]
-            #booking = Booking.objects.create(
-                #user=self.request.user,
-                #room=room,
-                #check_in=data['check_in'],
-                #check_out=data['check_out']
-            #)
-            #booking.save()
-            #return HttpResponse(booking)
-        #else:
-           # return HttpResponse('All of this category of rooms are booked!! Book another one')
+        
+class CancelBookingView(DeleteView):
+    model = Booking
+    template_name = 'booking_cancel_view.html'
+    success_url = reverse_lazy('hotel:BookingListView')
+    
