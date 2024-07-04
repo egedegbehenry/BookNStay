6e6f38e6a33c90a92406214940ed074a9e8e96d7
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-04 04:06:23 +0300

    cleaning and editing views.py

--- hotel/views.py
+++ hotel/views.py
@@ -1,40 +1,3 @@
-""" from typing import Any
-from django.db.models.query import QuerySet
-from django.http import JsonResponse
-from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, FormView, View, DeleteView
-from django.urls import reverse, reverse_lazy
-from .models import Room, Booking
-from .forms import AvailabilityForm
-from hotel.booking_functions.availability import check_availability
-from hotel.booking_functions.get_room_cat_url_list import get_room_cat_url_list
-from hotel.booking_functions.get_room_category_human_format import get_room_category_human_format
-from hotel.booking_functions.get_available_rooms import get_available_rooms
-from hotel.booking_functions.book_room import book_room
-# Create your views here.
-
-
-def RoomListView(request):
-    room_category_url_list = get_room_cat_url_list()
-
-    context = {
-        "room_list": room_category_url_list,
-    }
-    return render(request, 'room_list_view.html', context)
-
-class BookingListView(ListView):
-    model=Booking
-    template_name="booking_list_view.html"
-
-
-    def get_queryset(self, *args, **kwargs):
-        if self.request.user.is_staff:
-            booking_list = Booking.objects.all()
-            return booking_list
-        else:
-            booking_list = Booking.objects.filter(user=self.request.user)
-            return booking_list
-
 
 class RoomDetailView(View):
     def get (self, request, *args, **kwargs):
@@ -78,8 +41,6 @@ class CancelBookingView(DeleteView):
     model = Booking
     template_name = 'booking_cancel_view.html'
     success_url = reverse_lazy('hotel:BookingListView')
-    
- """
 
 
 
@@ -87,8 +48,9 @@ class CancelBookingView(DeleteView):
 
 
 
+"""
 
-""" from django.urls import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Room, Booking
 from .forms import RoomForm, BookingForm
@@ -138,4 +100,4 @@ class BookingDeleteView(DeleteView):
     model = Booking
     template_name = 'booking_confirm_delete.html'
     success_url = reverse_lazy('booking_list')
- """
+    """
