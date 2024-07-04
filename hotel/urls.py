b8c2b3a65f14ac1436efc61cbe001ffd3c24eb28
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-04 03:49:49 +0300

    cleaning and editing urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,27 +1,4 @@
-""" from django.urls import path
-from .views import RoomListView, BookingListView, RoomDetailView, CancelBookingView, CheckoutView, success_view, cancel_view, BookingFormView, contact_us
-
-app_name = 'hotel'
-
-urlpatterns = [
-
-    path('', BookingFormView.as_view(), name='BookingFormView'),
-    path('booking_list/', BookingListView.as_view(), name='BookingListView'),
-    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView'),
-    path('booking/cancel/<pk>', CancelBookingView.as_view(), name='CancelBookingView'),
-    path('checkout/', CheckoutView.as_view(), name='CheckoutView'),
-    path('success/', success_view, name='success_view'),
-    path('cancel/', cancel_view, name='cancel_view'),
-    path('contact-us/', contact_us, name="contact_us")
-
-
-]
- """
-
-
-
-
-""" from django.urls import path
+from django.urls import path
 from .views import (
     RoomListView, RoomCreateView, RoomUpdateView, RoomDeleteView,
     BookingListView, BookingCreateView, BookingUpdateView, BookingDeleteView
@@ -38,4 +15,3 @@ urlpatterns = [
     path('bookings/<int:pk>/edit/', BookingUpdateView.as_view(), name='booking_edit'),
     path('bookings/<int:pk>/delete/', BookingDeleteView.as_view(), name='booking_delete'),
 ]
- """
