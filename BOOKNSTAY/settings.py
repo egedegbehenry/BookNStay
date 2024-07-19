558e50437eb5eb517108dc25f058df2c51e0581f
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-19 04:08:11 +0300

    update

--- BOOKNSTAY/settings.py
+++ BOOKNSTAY/settings.py
@@ -104,6 +104,8 @@ DATABASES = {
     'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
+#DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
