773b92a52e1ad1c376cd8f743f67a6c9b48b285f
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-19 13:12:18 +0300

    update

--- BOOKNSTAY/settings.py
+++ BOOKNSTAY/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 # Default message storage backend
