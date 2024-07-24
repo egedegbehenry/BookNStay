124314c3bd47d970c5ff6dc21e2d8a73bb324a93
Henry Egedegbe <egedegbehenry@gmail.com>
2024-07-25 00:54:38 +0300

    turn debug to true

--- BOOKNSTAY/settings.py
+++ BOOKNSTAY/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-egedegbehenry-booknstay-0bpqw0tupam.ws-eu114.gitpod.io', 
